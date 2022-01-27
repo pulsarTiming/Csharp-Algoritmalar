d7a0ba844c7dd8d6ec417556615402f28c581f41
Merve <86164486+pulsarTiming@users.noreply.github.com>
2022-01-27 21:47:36 +0300

    Add files via upload

---
+++ herhangi_iki_sayinin_toplami/herhangi_iki_sayinin_toplami/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herhangi_iki_sayinin_toplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kaç sayı girmek istersiniz: ");
+            int tane = Convert.ToInt16(Console.ReadLine());
+
+            int[] liste = new int[tane];
+            for(int i = 0; i<tane; i++)
+            {
+                Console.Write((i + 1)+ ". sayıyı girin: ");
+                liste[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            Console.WriteLine("aradığınız sayıyı giriniz: ");
+            int aranan = Convert.ToInt16(Console.ReadLine());
+
+            for(int i = 0; i<tane; i++)
+            {
+                for(int j = (i+1); j < tane; j++)
+                {
+                    if(liste[i] + liste[j] == aranan)
+                    {
+                        Console.WriteLine(aranan + " sayısı listenin elemanlarından ikisinin toplamıdır. Bunlar = " + liste[i] + " ve " + liste[j]);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
