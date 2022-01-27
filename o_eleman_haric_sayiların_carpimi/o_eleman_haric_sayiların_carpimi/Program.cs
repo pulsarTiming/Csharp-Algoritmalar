6d6684f093248fa68b62f3db152e9353950f482a
Merve <86164486+pulsarTiming@users.noreply.github.com>
2022-01-27 22:29:34 +0300

    Add files via upload

---
+++ o_eleman_haric_sayiların_carpimi/o_eleman_haric_sayiların_carpimi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace o_eleman_haric_sayiların_carpimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liste = new int[5];
+            int[] yeniListe = new int[5];
+
+            for (int i = 0; i < liste.Length; i++)
+            {
+                Console.Write(i + 1 + ". değeri giriniz: ");
+                liste[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            int carpim = 1;
+            for(int i = 0; i<5; i++)
+            {
+                
+                for(int j = 0; j<5; j++)
+                {
+                    
+                    if(!(i == j))
+                    {
+                        carpim = carpim * liste[j];
+                    }
+                    
+                }
+                yeniListe[i] = carpim;
+                carpim = 1;
+            }
+
+            Console.WriteLine("\n***************************\n");
+
+             for(int i = 0; i<liste.Length; i++)
+            {
+                Console.WriteLine(yeniListe[i]);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
